47a44da6a15af8084e56223e94c41e0f206327d5
ollo69 <60491700+ollo69@users.noreply.github.com>
2020-09-26 15:28:31 +0200

    Minor fix
    - Fixed smartthings error when some device information are unaivailable (issue #40)

--- custom_components/samsungtv_smart/api/smartthings.py
+++ custom_components/samsungtv_smart/api/smartthings.py
@@ -322,13 +322,16 @@ class SmartThingsTV:
 
         _LOGGER.debug(data)
 
+        dev_data = data.get("main", {})
         # device_state = data['main']['switch']['value']
-        device_volume = data["main"]["volume"]["value"]
-        device_muted = data["main"]["mute"]["value"]
-        device_all_sources = json.loads(data["main"]["supportedInputSources"]["value"])
-        device_source = data["main"]["inputSource"]["value"]
-        device_tv_chan = data["main"]["tvChannel"]["value"]
-        device_tv_chan_name = data["main"]["tvChannelName"]["value"]
+        device_volume = dev_data.get("volume", {}).get("value", 0)
+        device_muted = dev_data.get("mute", {}).get("value", "")
+        device_all_sources = json.loads(
+            dev_data.get("supportedInputSources", {}).get("value", {})
+        )
+        device_source = dev_data.get("inputSource", {}).get("value", "")
+        device_tv_chan = dev_data.get("tvChannel", {}).get("value", "")
+        device_tv_chan_name = dev_data.get("tvChannelName", {}).get("value", "")
 
         if device_volume and device_volume.isdigit():
             self._volume = int(device_volume) / 100
